6e58d776ad959eb8f5fde6d41e3674199d0840eb
mannmanjeet988 <mannmanjeet988@gmail.com>
2024-03-07 16:02:54 +0530

    initial commit

--- src/pages/TrialPage/TrialPge.js
+++ src/pages/TrialPage/TrialPge.js
@@ -1,12 +1,29 @@
 import React, {useState, useEffect } from "react";
+import axios from "axios";
 
 const TrialPage = ()=>{
 
     const [tokenVal, setTokenVal] = useState("");
+    const[data,setData] = useState({})
+
+
         useEffect(() => {         
         let storedToken = (localStorage.getItem("token"));  
-        setTokenVal(storedToken);    
-        }, [])
+        setTokenVal(storedToken); 
+        if(tokenVal)   {
+           // https://api-test.myliveeye.com/api/cust-portal/customer/user-details')
+           axios.get("https://api-test.myliveeye.com/api/cust-portal/customer/user-details", {
+                headers: {
+                    "Authorization":  `Bearer ${storedToken}`
+                }  
+              }).then(response => setData(response))
+              .catch(err => console.log(err))
+              console.log("data", data);
+
+          }
+        },[tokenVal])
+            
+     
 
         return(
             <div>
